4eda7666839e0964665a81767d8d2bd5dbfca8d0
lupingsu236 <72119175+lupingsu236@users.noreply.github.com>
2021-07-21 20:03:40 +0800

    increased json limit

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const newsRouter = require("./router/newsRouter");
 const userRouter = require("./router/userRouter");
 
 const app = require("./app");
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({limit: '10mb'}));
+app.use(express.urlencoded({ extended: true, limit: '10mb'}));
 app.use(cors());
 app.use(morgan('combined'));
 
